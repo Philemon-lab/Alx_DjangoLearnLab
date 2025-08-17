9e258bd305f4d1cb5fd28b6d6b155b47acbe8aa7
philemon-lab <umpireltd@gmail.com>
2025-08-17 21:47:53 +0200

    Adding changes to query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,11 +2,12 @@ from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)      # Must match exactly
+    return Book.objects.filter(author=author)          # Must match exactly
 
 # List all books in a library
 def books_in_library(library_name):
-    library = Library.objects.get(name=library_name)  # Must match exactly
+    library = Library.objects.get(name=library_name)
     return library.books.all()
 
 # Retrieve the librarian for a library
