8a5a5b7074f8c5cc0af817f8212fe3445368c39f
philemon-lab <umpireltd@gmail.com>
2025-08-20 13:31:33 +0200

    Adding relationship_app/register.htmlfile

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,4 +14,4 @@ def register(request):
             return redirect('login')  # redirect to login after successful registration
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'relationship_app/register.html', {'form': form})
