251032394a401548b9625cce5349b05b20de1c4e
philemon-lab <umpireltd@gmail.com>
2025-08-20 20:14:17 +0200

    fixing BookList.as_view

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,4 +4,5 @@ from .views import BookListCreate, BookDetail
 urlpatterns = [
     path('books/', BookListCreate.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
+    path('books/,BookList.as_view(), name='book-list'),
 ]
