14672fc245aa9d06744877abc2f3ef1061357f61
philemon-lab <umpireltd@gmail.com>
2025-08-20 15:50:31 +0200

    Implemet customUser model, Admin integration, and settings update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,22 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    published_date = models.DateField()
-    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can edit book"),
-            ("can_delete_book", "Can delete book"),
-        ]
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.username
