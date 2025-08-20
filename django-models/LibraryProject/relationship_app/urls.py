363721e43892a39b176f4b09f9dea86fa06fbfb9
philemon-lab <umpireltd@gmail.com>
2025-08-20 13:24:36 +0200

    Adding urls.py file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
+    path('register/', views.register, name='register'),
+
 ]
