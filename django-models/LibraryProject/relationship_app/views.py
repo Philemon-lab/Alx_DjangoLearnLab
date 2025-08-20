fa0967050eaa338048764c04128abdd30b17ce0c
philemon-lab <umpireltd@gmail.com>
2025-08-20 13:26:51 +0200

    Adding urls.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth import login
 
 # Existing views (list_books, library_detail, role-based views, etc.)
 # ...
