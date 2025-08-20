fd85b26ac8e7087220739a35eb419a71150f6547
philemon-lab <umpireltd@gmail.com>
2025-08-20 20:23:30 +0200

    Adding Include to path

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookListCreate, BookDetail
 from .views import BookList
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('books/', BookListCreate.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
     path('books/',BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
