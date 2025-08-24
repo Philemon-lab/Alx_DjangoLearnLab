58856c4b19ec8ddbe298e5652ddc6deb94431a43
philemon-lab <umpireltd@gmail.com>
2025-08-24 22:01:19 +0200

    Create a view in the posts app that generates a feed based on the posts from users that the current user follows

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -9,10 +9,18 @@ from .serializers import PostSerializer, CommentSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permission_IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+    
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 def feed(request):
     user = request.user
