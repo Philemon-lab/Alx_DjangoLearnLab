bc0e3aa859bb9d3638140fdfa52eeb64a17d260f
Philemon-lab <umpireltd@gmail.com>
2025-09-02 23:13:18 +0200

    Fixing advanced_api_project/settings.py

--- advanced-api-project/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AuthorListCreateView, AuthorDetailView, BookListCreateView, BookDetailView
-
-urlpatterns = [
-    path('authors/', AuthorListCreateView.as_view(), name='author-list'),
-    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
-    path('books/', BookListCreateView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-]
